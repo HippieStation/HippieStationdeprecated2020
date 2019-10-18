72ad1b8a5a2c638554ed40baaaae643cf6fb8495
Rob Bailey <actioninja@gmail.com>
2019-10-18 10:22:18 -0700

    Commit

--- tgui-next/packages/tgui/index.js
+++ tgui-next/packages/tgui/index.js
@@ -109,8 +109,6 @@ const setupApp = () => {
   // Find data in the page, load inlined state.
   const holder = document.getElementById('data');
   const ref = holder.getAttribute('data-ref');
-  const stateJson = holder.textContent;
-  const state = JSON.parse(stateJson);
 
   // Initialize logger
   setLoggerRef(ref);
@@ -126,12 +124,6 @@ const setupApp = () => {
     // Backend update dispatches a store action
     store.dispatch(backendUpdate(state));
   };
-
-  // Render the app
-  if (state.config) {
-    logger.log('found inlined state');
-    store.dispatch(backendUpdate(state));
-  }
 
   // Enable hot module reloading
   if (module.hot) {
