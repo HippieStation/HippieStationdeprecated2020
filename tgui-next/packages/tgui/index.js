33b2e7293ec5d2fba79ed5a563d695ae37d4e6ad
Daniil Zaikin <dan.zaikin@gmail.com>
2019-11-08 16:37:30 +1000

    tgui-next

--- tgui-next/packages/tgui/index.js
+++ tgui-next/packages/tgui/index.js
@@ -1,20 +1,20 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import './polyfills';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import "./polyfills";
 
-import { loadCSS } from 'fg-loadcss';
-import { render } from 'inferno';
-import { setupHotReloading } from 'tgui-dev-server/link/client';
-import { backendUpdate } from './backend';
-import { tridentVersion } from './byond';
-import { setupDrag } from './drag';
-import { createLogger } from './logging';
-import { getRoute } from './routes';
-import { createStore } from './store';
+import { loadCSS } from "fg-loadcss";
+import { render } from "inferno";
+import { setupHotReloading } from "tgui-dev-server/link/client";
+import { backendUpdate } from "./backend";
+import { tridentVersion } from "./byond";
+import { setupDrag } from "./drag";
+import { createLogger } from "./logging";
+import { getRoute } from "./routes";
+import { createStore } from "./store";
 
 const logger = createLogger();
 const store = createStore();
-const reactRoot = document.getElementById('react-root');
+const reactRoot = document.getElementById("react-root");
 
 let initialRender = true;
 let handedOverToOldTgui = false;
@@ -26,20 +26,20 @@ const renderLayout = () => {
   }
   // Mark the beginning of the render
   let startedAt;
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV !== "production") {
     startedAt = Date.now();
   }
   try {
     const state = store.getState();
     // Initial render setup
     if (initialRender) {
-      logger.log('initial render', state);
+      logger.log("initial render", state);
 
       // ----- Old TGUI chain-loader: begin -----
       const route = getRoute(state);
       // Route was not found, load old TGUI
       if (!route) {
-        logger.info('loading old tgui');
+        logger.info("loading old tgui");
         // Short-circuit the renderer
         handedOverToOldTgui = true;
         // Unsubscribe from updates
@@ -47,20 +47,20 @@ const renderLayout = () => {
         // Load old TGUI using redirection method for IE8
         if (tridentVersion <= 4) {
           setTimeout(() => {
-            location.href = 'tgui-fallback.html?ref=' + window.__ref__;
+            location.href = "tgui-fallback.html?ref=" + window.__ref__;
           }, 10);
           return;
         }
         // Inject current state into the data holder
-        const holder = document.getElementById('data');
+        const holder = document.getElementById("data");
         holder.textContent = JSON.stringify(state);
         // Load old TGUI by injecting new scripts
-        loadCSS('v4shim.css');
-        loadCSS('tgui.css');
-        const head = document.getElementsByTagName('head')[0];
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = 'tgui.js';
+        loadCSS("v4shim.css");
+        loadCSS("tgui.css");
+        const head = document.getElementsByTagName("head")[0];
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "tgui.js";
         head.appendChild(script);
         // Bail
         return;
@@ -71,15 +71,14 @@ const renderLayout = () => {
       setupDrag(state);
     }
     // Start rendering
-    const { Layout } = require('./layout');
+    const { Layout } = require("./layout");
     const element = <Layout state={state} dispatch={store.dispatch} />;
     render(element, reactRoot);
-  }
-  catch (err) {
-    logger.error('rendering error', err);
+  } catch (err) {
+    logger.error("rendering error", err);
   }
   // Report rendering time
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV !== "production") {
     const finishedAt = Date.now();
     const diff = finishedAt - startedAt;
     const diffFrames = (diff / 16.6667).toFixed(2);
@@ -99,9 +98,8 @@ const renderLayout = () => {
 const parseStateJson = json => {
   try {
     return JSON.parse(json);
-  }
-  catch (err) {
-    logger.error('JSON parsing error: ' + err.message + '\n' + json);
+  } catch (err) {
+    logger.error("JSON parsing error: " + err.message + "\n" + json);
     throw err;
   }
 };
@@ -122,7 +120,7 @@ const setupApp = () => {
   // Enable hot module reloading
   if (module.hot) {
     setupHotReloading();
-    module.hot.accept(['./layout', './routes'], () => {
+    module.hot.accept(["./layout", "./routes"], () => {
       renderLayout();
     });
   }
@@ -137,19 +135,59 @@ const setupApp = () => {
   }
 
   // Dynamically load font-awesome from browser's cache
-  loadCSS('font-awesome.css');
+  loadCSS("font-awesome.css");
 };
 
 // Wait for DOM to properly load on IE8
 if (tridentVersion <= 4) {
-  if (document.readyState !== 'loading') {
+  if (document.readyState !== "loading") {
     setupApp();
-  }
-  else {
-    document.addEventListener('DOMContentLoaded', setupApp);
+  } else {
+    document.addEventListener("DOMContentLoaded", setupApp);
   }
 }
 // Load right away on all other browsers
 else {
   setupApp();
 }
+
+// hippie -- Yogs Key passthrough - so you can still do spaceman things when focused on a browser window
+
+if (document.addEventListener && window.location) {
+  // hey maybe some bozo is still using mega-outdated IE
+  let anti_spam = []; // wow I wish I could use e.repeat but IE is dumb and doesn't have it.
+  document.addEventListener("keydown", e => {
+    if (
+      e.target
+      && (e.target.localName === "input" || e.target.localName === "textarea")
+    ) {
+      return;
+    }
+    if (e.defaultPrevented) {
+      return;
+    } // do e.preventDefault() to prevent this behavior.
+    if (e.which) {
+      if (!anti_spam[e.which]) {
+        anti_spam[e.which] = true;
+        window.location.href = "?__keydown=" + e.which;
+      }
+    }
+  });
+  document.addEventListener("keyup", e => {
+    if (
+      e.target
+      && (e.target.localName === "input" || e.target.localName === "textarea")
+    ) {
+      return;
+    }
+    if (e.defaultPrevented) {
+      return;
+    }
+    if (e.which) {
+      anti_spam[e.which] = false;
+      window.location.href = "?__keyup=" + e.which;
+    }
+  });
+}
+
+// hippie end
