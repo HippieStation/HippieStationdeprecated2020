f154461b10828fda1d70bf36072875fece01725c
skoglol <33292112+kriskog@users.noreply.github.com>
2019-12-05 02:27:22 +0100

    tgui-next: Satellite Control (#48117)
    * tgui-next: Satellite Control
    
    * lint fix + rebuild

--- tgui-next/packages/tgui/routes.js
+++ tgui-next/packages/tgui/routes.js
@@ -56,6 +56,7 @@ import { PortableGenerator } from './interfaces/PortableGenerator';
 import { PowerMonitor } from './interfaces/PowerMonitor';
 import { Radio } from './interfaces/Radio';
 import { RapidPipeDispenser } from './interfaces/RapidPipeDispenser';
+import { SatelliteControl } from './interfaces/SatelliteControl';
 import { ScannerGate } from './interfaces/ScannerGate';
 import { ShuttleManipulator } from './interfaces/ShuttleManipulator';
 import { SmartVend } from './interfaces/SmartVend';
@@ -323,6 +324,10 @@ const ROUTES = {
   rpd: {
     component: () => RapidPipeDispenser,
     scrollable: true,
+  },
+  sat_control: {
+    component: () => SatelliteControl,
+    scrollable: false,
   },
   scanner_gate: {
     component: () => ScannerGate,
