AzriAnDr/HowlingVoid

tgui-next: useBackend edition (#48010)
* Initial useBackend implementation

* Lots of random ui shit

* Bulldozer

* Fix an infinite recursion

* Quotes

* Rebuild tgui

tgui-next/packages/tgui/interfaces/ChemDispenser.js
@@ -1,13 +1,11 @@
 import { toFixed } from 'common/math';
 import { toTitleCase } from 'common/string';
 import { Fragment } from 'inferno';
-import { act } from '../byond';
+import { useBackend } from '../backend';
 import { AnimatedNumber, Box, Button, Icon, LabeledList, ProgressBar, Section } from '../components';
 
 export const ChemDispenser = props => {
-  const { state } = props;
-  const { config, data } = state;
-  const { ref } = config;
+  const { act, data } = useBackend(props);
   const recording = !!data.recordingRecipe;
   // TODO: Change how this piece of shit is built on server side
   // It has to be a list, not a fucking OBJECT!
@@ -53,7 +51,7 @@ export const ChemDispenser = props => {
                 <Button
                   color="transparent"
                   content="Clear recipes"
-                  onClick={() => act(ref, 'clear_recipes')} />
+                  onClick={() => act('clear_recipes')} />
               </Box>
             )}
             {!recording && (
@@ -61,21 +59,21 @@ export const ChemDispenser = props => {
                 icon="circle"
                 disabled={!data.isBeakerLoaded}
                 content="Record"
-                onClick={() => act(ref, 'record_recipe')} />
+                onClick={() => act('record_recipe')} />
             )}
             {recording && (
               <Button
                 icon="ban"
                 color="transparent"
                 content="Discard"
-                onClick={() => act(ref, 'cancel_recording')} />
+                onClick={() => act('cancel_recording')} />
             )}
             {recording && (
               <Button
                 icon="floppy-o"
                 color="green"
                 content="Save"
-                onClick={() => act(ref, 'save_recording')} />
+                onClick={() => act('save_recording')} />
             )}
           </Fragment>
         )}>
@@ -85,7 +83,7 @@ export const ChemDispenser = props => {
             width="129.5px"
             lineHeight="21px"
             content={recipe.name}
-            onClick={() => act(ref, 'dispense_recipe', {
+            onClick={() => act('dispense_recipe', {
               recipe: recipe.name,
             })} />
         ))}
@@ -103,7 +101,7 @@ export const ChemDispenser = props => {
               icon="plus"
               selected={amount === data.amount}
               content={amount}
-              onClick={() => act(ref, 'amount', {
+              onClick={() => act('amount', {
                 target: amount,
               })} />
           ))
@@ -115,7 +113,7 @@ export const ChemDispenser = props => {
               width="129.5px"
               lineHeight="21px"
               content={chemical.title}
-              onClick={() => act(ref, 'dispense', {
+              onClick={() => act('dispense', {
                 reagent: chemical.id,
               })} />
           ))}
@@ -129,7 +127,7 @@ export const ChemDispenser = props => {
               icon="minus"
               disabled={recording}
               content={amount}
-              onClick={() => act(ref, 'remove', { amount })} />
+              onClick={() => act('remove', { amount })} />
           ))
         )}>
         <LabeledList>
@@ -140,7 +138,7 @@ export const ChemDispenser = props => {
                 icon="eject"
                 content="Eject"
                 disabled={!data.isBeakerLoaded}
-                onClick={() => act(ref, 'eject')} />
+                onClick={() => act('eject')} />
             )}>
             {recording
               && 'Virtual beaker'

HippieStation/HippieStationdeprecated2020

tgui-next/packages/tgui/interfaces/ChemDispenser.js
@@ -1,13 +1,11 @@
 import { toFixed } from 'common/math';
 import { toTitleCase } from 'common/string';
 import { Fragment } from 'inferno';
-import { act } from '../byond';
+import { useBackend } from '../backend';
 import { AnimatedNumber, Box, Button, Icon, LabeledList, ProgressBar, Section } from '../components';
 
 export const ChemDispenser = props => {
-  const { state } = props;
-  const { config, data } = state;
-  const { ref } = config;
+  const { act, data } = useBackend(props);
   // TODO: Change how this piece of shit is built on server side
   // It has to be a list, not a fucking OBJECT!
   const recipes = Object.keys(data.recipes)
@@ -38,12 +36,12 @@ export const ChemDispenser = props => {
               <Button
                 color="transparent"
                 content="Clear Recipes"
-                onClick={() => act(ref, 'clear_recipes')} />
+                onClick={() => act('clear_recipes')} />
             </Box>
             <Button
               icon="circle"
               content="Add Recipe"
-              onClick={() => act(ref, 'add_recipe')} />
+              onClick={() => act('add_recipe')} />
           </Fragment>
         )}>
         {recipes.map(recipe => (
@@ -52,7 +50,7 @@ export const ChemDispenser = props => {
             width="129.5px"
             lineHeight="21px"
             content={recipe.name}
-            onClick={() => act(ref, 'dispense_recipe', {
+            onClick={() => act('dispense_recipe', {
               recipe: recipe.contents,
             })} />
         ))}
@@ -70,7 +68,7 @@ export const ChemDispenser = props => {
               icon="plus"
               selected={amount === data.amount}
               content={amount}
-              onClick={() => act(ref, 'amount', {
+              onClick={() => act('amount', {
                 target: amount,
               })} />
           ))
@@ -82,7 +80,7 @@ export const ChemDispenser = props => {
               width="129.5px"
               lineHeight="21px"
               content={chemical.title}
-              onClick={() => act(ref, 'dispense', {
+              onClick={() => act('dispense', {
                 reagent: chemical.id,
               })} />
           ))}
@@ -95,7 +93,7 @@ export const ChemDispenser = props => {
             <Button key={amount}
               icon="minus"
               content={amount}
-              onClick={() => act(ref, 'remove', { amount })} />
+              onClick={() => act('remove', { amount })} />
           ))
         )}>
         <LabeledList>
@@ -106,7 +104,7 @@ export const ChemDispenser = props => {
                 icon="eject"
                 content="Eject"
                 disabled={!data.isBeakerLoaded}
-                onClick={() => act(ref, 'eject')} />
+                onClick={() => act('eject')} />
             )}>
             {data.isBeakerLoaded
               && (